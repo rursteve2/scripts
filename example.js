5c4a3f5e9faa32cad21c206355c29c47a99f8aaf
Stephen <rursteve2@yahoo.com>
2019-12-21 03:01:26 -0500

    changed stuff

--- example.js
+++ example.js
@@ -5,5 +5,5 @@ const puppeteer = require('puppeteer');
     const page = await browser.newPage()
     await page.goto("https://example.com")
     await page.screenshot({path: 'example.png'})
-    await browser.close()
+    await browser.close() 
 })
